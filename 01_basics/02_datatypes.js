d0a3f907a52439027e092cde715020d1108750ee
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-06-22 10:18:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+" use strict"; // treat all js code as never version
+console.log(3+3);
+
+//number 2 to power//
+//bigint 
+//string 
+//boolean=>true/false
+//null=>standalone value//
+//undefined=>
+    //symbol=>unique
+//object
+console.log(typeof"rahul");
+
