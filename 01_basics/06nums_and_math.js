5dd8b17213cb9929662203732b829fd829cab323
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-06-23 11:18:45 +0000

    num and math

---
+++ 01_basics/06nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+console.log(score);
+
+
+const balance  = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+ 
+const otherNumber = 123.87
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++MATH++++++++++//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.min(4,3,5,8,9));
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
+
+
+
+
