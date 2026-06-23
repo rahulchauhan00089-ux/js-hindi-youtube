00b1bbf1ab8b58b01ab3722b77f20f4b79bca6e6
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-06-23 05:19:49 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+//primitive 
+
+// 7types :string,number,boolean,null,undefined,symbol,bigInt
+const score = 100
+const scorevalue= 100.3 
+const isloggedin = false
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid) 
+
+//reference (non primitive)
+
+//array,objectsfunctions
+const heros = ['shaktiman','naagraj','doga'];
+let myobj = {
+    name:"hitesh",
+    age:22,
+}    
+
+
+ const myfunction = function(){
+ console.log("hello world");
+ } 
+
+
+
+
+
