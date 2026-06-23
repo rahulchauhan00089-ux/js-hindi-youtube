9d173ff247b166d5c595c4990ba4652c41c5532b
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-06-23 04:01:07 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+// let score = "rahul"
+// console.log(typeof score);
+// console.log(typeof score);
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+
+// "33"=>33
+// "33qgd"=>NaN
+// true=>1;
+// false=>0
+// 1 =>true
+//0=>false
+//"rahul"=>true
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
