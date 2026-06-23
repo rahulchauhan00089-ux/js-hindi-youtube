1dd646b2992c47fd289ef60cf781128ece32ebaf
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-06-23 09:25:58 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,26 @@
+const name = "rahui "
+const repocount= 50
+// console.log(hello my name is ${name} and my repo count is ${repocount};)
+
+const gameName = new String('cricket')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(4));
+
+const newstring = gameName.substring(0,4);
+console.log(newstring);
+
+const anotherstring = gameName.slice(-7,4);   // -ve value de sakte hai
+console.log(anotherstring);
+
+const newStringone = "    rahulchauhan    "
+
+console.log(newStringone);
+console.log(newStringone.trim);
+
+
+
+
+ 
