1b69de50e90db1311e56663fa2c65ce36c4ff283
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-07-09 07:10:16 +0000

    jsontalk

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,13 @@
+const course = {
+    courseName: "js in hindi",
+    coursePrice: 999,
+    courseInstructor: "CodeWithHarry",
+    
+}
+
+
+//course.courseInstructor 
+
+const{courseInstructor: instructor}=course
+console.log(courseInstructor);
+
