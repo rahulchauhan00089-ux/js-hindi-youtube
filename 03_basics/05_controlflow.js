a0243e03a19c2556932512a5537c50b58ee69ca0
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-07-17 03:28:09 +0000

    controlflow

---
+++ 03_basics/05_controlflow.js
@@ -0,0 +1,61 @@
+// if 
+if(2 =="2"){
+console.log("true");
+
+}
+
+if(2 ===4){
+console.log("true");
+
+}
+
+const temp = 34
+if(temp==34){
+    console.log("temp is 34");
+}
+
+else{
+    console.log("temp is not 34");
+}
+
+// const score =200
+// if(score>100){
+//     let  power = "fly"
+//     console.log(`you are super hero and your power is ${power}`);
+// }
+// console.log(`you are super hero and your power is ${power}`);
+
+
+// const balance = 100
+// if(balance>100) console.log("test")
+
+// const balance =1100
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance <750){
+//     console.log("less than 750");
+    
+// }
+// else if(balance <1200){
+//     console.log("less than 1200");
+    
+// }
+
+
+const userloggedin = true 
+const debitcard = true
+const loggedinfromgoogle=false
+const loggedinfromemail= true
+if(userloggedin && debitcard){
+    console.log("allow to buy course");
+    
+}
+
+if(loggedinfromgoogle|| loggedinfromemail){
+    console.log("user logged in");
+    
+}
+
+
