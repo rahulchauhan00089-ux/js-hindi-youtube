e28a8f2fba9d22035be5bb75c0610ac6f8fa2b1e
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-07-17 12:16:40 +0000

    do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+// while loop
+let index = 0
+while (index<=10){
+    console.log(`value of index${index}`);
+    index= index+2
+}
+
+// array ko loop se karna ho toh
+let myarray = ["rahul","ffy","gtsy"]
+let arr = 0
+while (arr<myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr=arr+1
+    
+}
+
+let score =1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=10);
