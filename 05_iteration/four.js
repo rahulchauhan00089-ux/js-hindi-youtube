c4fe98ee9fe720953329c2ec317f1ce50af725bc
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-07-22 11:41:25 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    'game1':'nfs',
+    'game2':'spiderman',
+    'swift':'swift by apple'
+} 
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+}
+
+const programming = ["js","rb","py","java"]
+for (const key in programming) {
+    console.log(programming[key]);
+    
+    
+// const map = new Map()
+// map.set('IN', "india")
+// map.set('usa',"united state of america")
+// map.set('fr',"france")
+
+// for (const key in map) {
+//     console.log(key);
+    
+   
+    
+}
+
+    
