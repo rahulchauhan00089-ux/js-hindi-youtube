e84cc9ed109a39403892dbc372e6dad6483a50b8
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-07-22 12:17:41 +0000

    for each

---
+++ 05_iteration/five.js
@@ -0,0 +1,30 @@
+
+// for each
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach(function(val){
+//        console.log(val);    
+
+// })
+
+
+//  arrow function
+
+// coding.forEach( (item)=>{
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printme)
+
+
+coding.forEach((item,index,arr) => {
+    console.log(item,index,arr);
+    
+}
+)
