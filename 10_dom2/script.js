48182576b993aae27066b5b796eb84139f7df49c
rahulchauhan00089-ux <rahulchauhan00089@gmail.com>
2026-08-26 11:29:04 +0000

    dom2

---
+++ 10_dom2/script.js
@@ -0,0 +1,25 @@
+//  let heading = document.getElementById("heading")
+//  console.dir(heading);
+ 
+// let headings = document.getElementsByClassName("heading-class");
+// console.log(headings);
+// console.dir(headings);
+
+// let paragraph = document.getElementsByTagName("p");
+// console.dir(paragraph);
+
+//  let firelements = document.querySelector("p"); // first element 
+//  console.dir(firelements);
+
+//   let allelements = document.querySelectorAll("p");// all element
+//  console.dir(allelements);
+ 
+// let firelements = document.querySelector(".myClass"); // first element 
+//  console.dir(firelements);
+
+//   let allelements = document.querySelectorAll(".myClass");// all element
+//  console.dir(allelements);
+ 
+ let firelements = document.querySelector("#myId"); // first element 
+  console.dir(firelements);
+
